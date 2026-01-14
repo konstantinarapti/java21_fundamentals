9db1d48a036ee14855fa6b20987bd0a72aad9d32
Konstantina Rapti <konstantinarapti@Mac.home>
2026-01-14 18:43:31 +0200

    Add java Basic Streams use example with map and filter

---
+++ src/main/java/com/example/java21/streams/StreamBasics.java
@@ -0,0 +1,15 @@
+package src.main.java.com.example.java21.streams;
+
+import java.util.List;
+
+public class StreamBasics {
+
+    public static void main(String[] args) {
+        List<String> names = List.of("Anna", "Alex", "John", "Konstantina");
+
+        names.stream()
+                .filter(name -> name.startsWith("A"))
+                .map(String::toUpperCase)
+                .forEach(System.out::println);
+    }
+}
