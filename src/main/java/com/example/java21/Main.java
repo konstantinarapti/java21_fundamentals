ff25101c6b8446e8918784efd1fdb2c85fd57cc4
Konstantina Rapti <konstantinarapti@Mac.home>
2026-01-14 18:21:11 +0200

    Initial project Java 21 setup with Main class

---
+++ src/main/java/com/example/java21/Main.java
@@ -0,0 +1,8 @@
+package src.main.java.com.example.java21;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Java 21 fundamentals project");
+    }
+}
